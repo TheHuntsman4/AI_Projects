59f3a8a82f5acdd17d7015f5e696c986659a50b5
TheHuntsman4 <anikethvij464@gmail.com>
2023-11-02 13:29:23 +0530

    <feat>:[tries adding a page when the video si done processing]

--- 1/video_processing/video_app/views.py
+++ 1/video_processing/video_app/views.py
@@ -4,13 +4,14 @@ import pandas as pd
 import numpy as np
 from ultralytics import YOLO
 import io
-from django.http import StreamingHttpResponse
+from django.http import StreamingHttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 class ProcessVideo(threading.Thread):
-    def __init__(self, video_file):
+    def __init__(self, video_file, callback):
         threading.Thread.__init__(self)
         self.video_file = video_file
+        self.callback = callback
 
     def run(self):
         video_filename = 'temp_video.mp4'
@@ -27,6 +28,9 @@ class ProcessVideo(threading.Thread):
             count += 1
             if count % 24 != 0:
                 continue
+            if not ret:
+                self.callback()  # Trigger the callback function when the video ends
+                break
             frame = cv2.resize(frame, (1020, 600))
             results = model.predict(frame)
             a=results[0].boxes.data
@@ -43,17 +47,20 @@ class ProcessVideo(threading.Thread):
                     cv2.putText(frame, str('person'), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
                     person_count += 1
             cv2.putText(frame, 'Number of people: ' + str(person_count), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                        (255, 255, 255), 2)
+                        (0, 255, 255), 2)
             _, encodedImage = cv2.imencode('.jpg', frame)
             yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + encodedImage.tobytes() + b'\r\n')
 
 def index(request):
     return render(request, 'index.html')
 
+def end(request):
+    return render(request, 'end.html')
+
 def upload_video(request):
     if request.method == 'POST':
         video_file = request.FILES['file']
-        new_thread = ProcessVideo(video_file)
+        new_thread = ProcessVideo(video_file, callback=lambda: HttpResponseRedirect('/end'))
         new_thread.start()
         return StreamingHttpResponse(new_thread.run(), content_type='multipart/x-mixed-replace; boundary=frame')
     return render(request, 'upload.html')
--- 1/video_processing/video_processing/urls.py
+++ 1/video_processing/video_processing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from video_app.views import index, upload_video
+from video_app.views import index, upload_video,end
 
 urlpatterns = [
     path('', index, name='index'),
     path('main', upload_video, name='upload_video'),
+    path('end',end,name="end")
 ]
